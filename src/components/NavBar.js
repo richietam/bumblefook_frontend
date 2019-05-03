e157cd9e2a1f99de5a18c411fca9471b8633cbdf
richietam <richardtam.ny@gmail.com>
2019-05-03 16:06:52 -0400

    navbar component update

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,28 +1,43 @@
-import React from 'react'
-import Nav from 'react-bootstrap/Nav'
+import React, { Component } from 'react'
+import { Input, Menu, Container } from 'semantic-ui-react'
 
-const NavBar = props => {
-  return (
-    <Nav
-      activeKey="/home"
-      onSelect={selectedKey => alert(`selected ${selectedKey}`)}
-    >
-      <Nav.Item>
-        <Nav.Link href="/home">Active</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link eventKey="link-1">Link</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link eventKey="link-2">Link</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link eventKey="disabled" disabled>
-          Disabled
-        </Nav.Link>
-      </Nav.Item>
-    </Nav>
-  )
+export default class MenuExampleSecondary extends Component {
+
+  state = { activeItem: 'home' }
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+  render() {
+
+    const { activeItem } = this.state
+
+    return (
+      <Container id="mainNav">
+      <Menu secondary id="mainNav">
+        <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick} />
+        <Menu.Item
+          name='Discover'
+          active={activeItem === 'Discover'}
+          onClick={this.handleItemClick}
+        />
+        <Menu.Item
+          name='Favorites'
+          active={activeItem === 'Favorites'}
+          onClick={this.handleItemClick}
+        />
+        <Menu.Menu position='right'>
+          <Menu.Item>
+            <Input icon='search' placeholder='Search...' />
+          </Menu.Item>
+          <Menu.Item
+            name='logout'
+            active={activeItem === 'logout'}
+            onClick={this.handleItemClick}
+          />
+        </Menu.Menu>
+      </Menu>
+      </Container>
+    )
+  }
+
 }
-
-export default NavBar
